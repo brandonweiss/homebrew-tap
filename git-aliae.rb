4b1554427d67962a2c2034354701d4ac22d771c6
Brandon Weiss <brandon@anti-pattern.com>
2014-02-18 23:07:54 -0800

    Creating a formula for Conrad Irwin's git-aliae

---
+++ git-aliae.rb
@@ -0,0 +1,10 @@
+require "formula"
+
+class GitAliae < Formula
+  homepage "https://github.com/ConradIrwin/git-aliae"
+  head     "https://github.com/ConradIrwin/git-aliae.git"
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+end
