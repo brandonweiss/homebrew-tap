85eee8a679bb3987dd08b01d54df17054ae1efdd
Brandon Weiss <brandon@anti-pattern.com>
2014-05-02 17:29:39 -0700

    Improving formatting

--- git-aliae.rb
+++ git-aliae.rb
@@ -1,10 +1,12 @@
 require "formula"
 
 class GitAliae < Formula
+
   homepage "https://github.com/ConradIrwin/git-aliae"
   head     "https://github.com/ConradIrwin/git-aliae.git"
 
   def install
     bin.install Dir["bin/*"]
   end
+
 end
